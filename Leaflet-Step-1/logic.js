87d2cc2fe1686b080cacdbb975be9cca6cdf31a9
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-05-04 23:38:35 -0500

    Finished part 1

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -21,7 +21,29 @@ d3.json(queryUrl).then(function(data) {
     createFeatures(data.features, myMap);
   });
 function createFeatures(earthquakeData, myMap) {
-
+    function colorchooser(depth) {
+        var color="";
+        if(depth < -10){
+            color = "lime";
+        }
+        else if (depth < 10)
+        {
+            color = "green";
+        }
+        else if (depth < 30)
+        {
+            color = "yellow";
+        }
+        else if (depth < 50)
+        {
+            color = "orange";
+        }
+        else 
+        {
+            color = "red";
+        }
+        return color;
+    }
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature) {
@@ -29,7 +51,8 @@ function createFeatures(earthquakeData, myMap) {
       L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]], {
         fillOpacity: 0.75,
         color: "white",
-        fillColor: "blue",
+        weight: 1,
+        fillColor: colorchooser(feature.geometry.coordinates[2]),
         // Adjust radius
         radius: feature.properties.mag * 20000
       }).bindPopup("<h3>" + feature.properties.place +
@@ -45,86 +68,4 @@ function createFeatures(earthquakeData, myMap) {
     // Sending our earthquakes layer to the createMap function
 }
   
-  // Country data
-  var countries = [
-    {
-      name: "Brazil",
-      location: [-14.2350, -51.9253],
-      points: 227
-    },
-    {
-      name: "Germany",
-      location: [51.1657, 10.4515],
-      points: 218
-    },
-    {
-      name: "Italy",
-      location: [41.8719, 12.5675],
-      points: 156
-    },
-    {
-      name: "Argentina",
-      location: [-38.4161, -63.6167],
-      points: 140
-    },
-    {
-      name: "Spain",
-      location: [40.4637, -3.7492],
-      points: 99
-    },
-    {
-      name: "England",
-      location: [52.355, 1.1743],
-      points: 98
-    },
-    {
-      name: "France",
-      location: [46.2276, 2.2137],
-      points: 96
-    },
-    {
-      name: "Netherlands",
-      location: [52.1326, 5.2913],
-      points: 93
-    },
-    {
-      name: "Uruguay",
-      location: [-32.4228, -55.7658],
-      points: 72
-    },
-    {
-      name: "Sweden",
-      location: [60.1282, 18.6435],
-      points: 61
-    }
-  ];
-  
-  
-  // Loop through the cities array and create one marker for each city object
-  for (var i = 0; i < countries.length; i++) {
-  
-    // Conditionals for countries points
-    var color = "";
-    if (countries[i].points > 200) {
-      color = "yellow";
-    }
-    else if (countries[i].points > 100) {
-      color = "blue";
-    }
-    else if (countries[i].points > 90) {
-      color = "green";
-    }
-    else {
-      color = "red";
-    }
-  
-    // Add circles to map
-    L.circle(countries[i].location, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: color,
-      // Adjust radius
-      radius: countries[i].points * 1500
-    }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
-  }
   
